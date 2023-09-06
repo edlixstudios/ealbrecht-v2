d948e2b403deddeabed403c1954c40a1b62ca151
ealbrecht <edlixstudios@googlemail.com>
2023-09-06 11:23:34 +0200

    feat: moved DB from supabase to vercel

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import { useChangeLocale, useCurrentLocale, useI18n } from "$/locales/client";
+import React, { useTransition } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import DE from "country-flag-icons/react/3x2/DE";
 import US from "country-flag-icons/react/3x2/US";
+import { Loader2 } from "lucide-react";
 
 export const LocaleSwitcher = () => {
     const locale = useCurrentLocale();
     const t = useI18n();
     const changeLocale = useChangeLocale();
+    const [isPending, setTransition] = useTransition();
 
     function handleLanguageChange(value: string) {
-        changeLocale(value as typeof locale);
+        setTransition(() => changeLocale(value as typeof locale));
     }
+
+    if (isPending) return <Loader2 className="animate-spin w-7 h-7"></Loader2>;
 
     return (
         <Select defaultValue={locale} onValueChange={handleLanguageChange}>
