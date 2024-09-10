d62c3e75d2b7aed493a99a2e5264d5057091122e
Eduard Albrecht <eddysoulo91@gmail.com>
2024-09-10 09:44:25 +0200

    hope it works

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,15 +6,15 @@ import { LocaleSwitcher } from "./locale-switcher";
 const amatic = Amatic_SC({ subsets: ["latin"], weight: "700" });
 
 export default async function Header() {
-    const t = await getI18n();
+  const t = await getI18n();
 
-    return (
-        <header className="w-full py-4 px-[10vw] flex flex-col items-center justify-center absolute top-0 xl:flex-row xl:justify-between">
-            <div className="flex items-center gap-2">
-                <Image src={"/me.svg"} width={50} height={50} alt={"Me"} />
-                <h1 className={`${amatic.className} text-4xl`}>{"<ALBR<CHT.DEV"}</h1>
-            </div>
-            <LocaleSwitcher />
-        </header>
-    );
+  return (
+    <header className="w-full py-4 px-[10vw] flex flex-col items-center justify-center absolute top-0 xl:flex-row xl:justify-between">
+      <div className="flex items-center gap-2">
+        <Image src={"/me-logo.svg"} width={50} height={50} alt={"Me"} />
+        <h1 className={`${amatic.className} text-4xl`}>{"<ALBR<CHT.DEV"}</h1>
+      </div>
+      <LocaleSwitcher />
+    </header>
+  );
 }
